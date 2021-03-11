cec734d8babcce9fa5db2d09e53dea7f7deac3bb
ShkrobNastya <60944202+ShkrobNastya@users.noreply.github.com>
2021-03-11 22:57:48 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  let minim;
+  if (!array || (array.length === 0)) {
+    return 0;
+  } else {
+    minim = array[0];
+    for (let i=1; i<array.length; i++){
+      if (array[i]<minim) {
+        minim=array[i];
+      }
+    }
+    return minim;
+  }
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  let maxim;
+  if (!array || (array.length === 0)) {
+    return 0;
+  } else {
+    maxim = array[0];
+    for (let i=1; i<array.length; i++){
+      if (array[i]>maxim) {
+        maxim=array[i];
+      }
+    }
+    return maxim;
+  }
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  let sum=0;
+  if (!array || (array.length === 0)) {
+    return 0;
+  } else {
+    for (let i=0; i<array.length; i++){
+      sum+=array[i];
+    }
+    return sum/(array.length);
+  }
+};
